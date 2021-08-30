fc1744fa41797ab82e878f364fbf902c69cdbde8
subu00 <subash00934@gmail.com>
2021-08-30 19:15:57 +0545

    login functionality

---
+++ src/BeforeLogin.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function BeforeLogin() {
+    return (
+        <>Sippy is a dog </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState} from "react"
 import {render} from "react-dom"
-import BeforeLogin  from "./BeforeLogin";
+
 
 function App() {
-    return (
-        <> 
-            <h1>Hello Sippy</h1>
-            <BeforeLogin />
-        </>
-    )
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    if(loggedIn) {   // Here the user is loggedIn
+        return (
+            <> 
+                <h1>Welcome to our Application</h1>
+                <button onClick={() => setLoggedIn(false)}>Logout</button>
+            </>
+        )
+    } else {
+        return (
+            <> 
+                <h2>Please Login In</h2>
+                <button onClick={ () => setLoggedIn(true)}>Log In</button>
+            </>
+        )
+    }
+
 }
 
-render(<App />, document.querySelector("#root"))
+render(<App /> , document.querySelector("#root"))
